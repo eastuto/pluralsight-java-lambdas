08732137707c1c043711d56c473ff55351463fce
Eugene Astuto <eastuto@gmail.com>
2017-11-27 13:19:07 +1100

    Hackerrank unfairness challenge

---
+++ src/com/pluralsightlambdas/HRankUnfairnessTest.java
@@ -0,0 +1,28 @@
+package com.pluralsightlambdas;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+public class HRankUnfairnessTest {
+
+    public static void main(String[] args) {
+        int K = 3;
+        int N = 7;
+
+        List<Integer> list = new ArrayList<>(asList(1,
+                2,
+                3,
+                4,
+                10,
+                20,
+                30,
+                40,
+                100,
+                200));
+
+
+    }
+}
